64a1945e09296eefb8d6f380ce6f9ff75f923267
Gustavo Menezes <menezesgustavo1102@gmail.com>
2023-11-27 20:58:36 -0300

    atualizações serializer e views

--- autoapp/apps/vehicle/views.py
+++ autoapp/apps/vehicle/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Vehicle
+from rest_framework import viewsets
+from .serializer import VehicleSerializer
 
 # Create your views here.
+class VehicleViewSet(viewsets.ModelViewSet):
+    queryset = Vehicle.objects.all()
+    serializer_class = VehicleSerializer  
